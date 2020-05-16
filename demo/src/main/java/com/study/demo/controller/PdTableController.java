72be9d326c33f702df1eefbc497fff9801618079
Administrator <Administrator@192.168.11.2>
2020-05-16 12:10:35 +0900

    CrossOrigin 적용

--- demo/src/main/java/com/study/demo/controller/PdTableController.java
+++ demo/src/main/java/com/study/demo/controller/PdTableController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class PdTableController extends AbstractController{
 	@Autowired
 	PdTableService pdTableService;
 	
+	@CrossOrigin
 	@GetMapping("/pdlist")
 	public Map<String, Object> pdList(Map<String,Object> map) {
 		return makeQueryResultMap(pdTableService.pdList(map));
