365aacce9286b0c13055ec8aed65e0e610e7c1fc
82109 <82109@DESKTOP-V6RAQBA>
2020-07-11 16:57:22 +0900

    테스트입니다. vo파일

---
+++ demo/src/main/java/com/study/demo/VO/EmpMaster.java
@@ -0,0 +1,358 @@
+package com.study.demo.VO;
+
+//직원마스터 
+public class EmpMaster {
+
+ // 직원번호 
+ private Integer empNo;
+
+ // 이름 
+ private String empNm;
+
+ // 성별 
+ private String empGd;
+
+ // 직위 
+ private String empPs;
+
+ // 부서 
+ private String empDp;
+
+ // 입사일자 
+ private String empJoinDt;
+
+ // 퇴사일자 
+ private String empResnDt;
+
+ // 휴대폰번호 
+ private String empHp;
+
+ // 이메일주소 
+ private String empEmail;
+
+ // 주민등록번호 
+ private String empRnum;
+
+ // 사진 
+ private byte[] empPt;
+
+ // 연락처 
+ private String empTel;
+
+ // 권한코드 
+ private String empAuthCd;
+
+ // 비밀번호 
+ private String empPw;
+
+ // 생년월일 
+ private String empBirth;
+
+ // 양력음력 
+ private String empSl;
+
+ // 우편번호 
+ private String empZip;
+
+ // 주소 
+ private String empAddr;
+
+ // 주소2 
+ private String empAddr2;
+
+ // 연차 
+ private String empCrNo;
+
+ // 자격증사항 
+ private String empLc;
+
+ // 수습사원여부 
+ private String empPbYn;
+
+ // 비고 
+ private String empRemarks;
+
+ // 최초생성자 
+ private String regUser;
+
+ // 최초생성일시 
+ private String regDt;
+
+ // 최종수정자 
+ private String udtUser;
+
+ // 최종수정일시 
+ private String udtDt;
+
+ // 사용여부 
+ private String useYn;
+
+ // 계좌번호 
+ private String empAcc;
+
+ public Integer getEmpNo() {
+     return empNo;
+ }
+
+ public void setEmpNo(Integer empNo) {
+     this.empNo = empNo;
+ }
+
+ public String getEmpNm() {
+     return empNm;
+ }
+
+ public void setEmpNm(String empNm) {
+     this.empNm = empNm;
+ }
+
+ public String getEmpGd() {
+     return empGd;
+ }
+
+ public void setEmpGd(String empGd) {
+     this.empGd = empGd;
+ }
+
+ public String getEmpPs() {
+     return empPs;
+ }
+
+ public void setEmpPs(String empPs) {
+     this.empPs = empPs;
+ }
+
+ public String getEmpDp() {
+     return empDp;
+ }
+
+ public void setEmpDp(String empDp) {
+     this.empDp = empDp;
+ }
+
+ public String getEmpJoinDt() {
+     return empJoinDt;
+ }
+
+ public void setEmpJoinDt(String empJoinDt) {
+     this.empJoinDt = empJoinDt;
+ }
+
+ public String getEmpResnDt() {
+     return empResnDt;
+ }
+
+ public void setEmpResnDt(String empResnDt) {
+     this.empResnDt = empResnDt;
+ }
+
+ public String getEmpHp() {
+     return empHp;
+ }
+
+ public void setEmpHp(String empHp) {
+     this.empHp = empHp;
+ }
+
+ public String getEmpEmail() {
+     return empEmail;
+ }
+
+ public void setEmpEmail(String empEmail) {
+     this.empEmail = empEmail;
+ }
+
+ public String getEmpRnum() {
+     return empRnum;
+ }
+
+ public void setEmpRnum(String empRnum) {
+     this.empRnum = empRnum;
+ }
+
+ public byte[] getEmpPt() {
+     return empPt;
+ }
+
+ public void setEmpPt(byte[] empPt) {
+     this.empPt = empPt;
+ }
+
+ public String getEmpTel() {
+     return empTel;
+ }
+
+ public void setEmpTel(String empTel) {
+     this.empTel = empTel;
+ }
+
+ public String getEmpAuthCd() {
+     return empAuthCd;
+ }
+
+ public void setEmpAuthCd(String empAuthCd) {
+     this.empAuthCd = empAuthCd;
+ }
+
+ public String getEmpPw() {
+     return empPw;
+ }
+
+ public void setEmpPw(String empPw) {
+     this.empPw = empPw;
+ }
+
+ public String getEmpBirth() {
+     return empBirth;
+ }
+
+ public void setEmpBirth(String empBirth) {
+     this.empBirth = empBirth;
+ }
+
+ public String getEmpSl() {
+     return empSl;
+ }
+
+ public void setEmpSl(String empSl) {
+     this.empSl = empSl;
+ }
+
+ public String getEmpZip() {
+     return empZip;
+ }
+
+ public void setEmpZip(String empZip) {
+     this.empZip = empZip;
+ }
+
+ public String getEmpAddr() {
+     return empAddr;
+ }
+
+ public void setEmpAddr(String empAddr) {
+     this.empAddr = empAddr;
+ }
+
+ public String getEmpAddr2() {
+     return empAddr2;
+ }
+
+ public void setEmpAddr2(String empAddr2) {
+     this.empAddr2 = empAddr2;
+ }
+
+ public String getEmpCrNo() {
+     return empCrNo;
+ }
+
+ public void setEmpCrNo(String empCrNo) {
+     this.empCrNo = empCrNo;
+ }
+
+ public String getEmpLc() {
+     return empLc;
+ }
+
+ public void setEmpLc(String empLc) {
+     this.empLc = empLc;
+ }
+
+ public String getEmpPbYn() {
+     return empPbYn;
+ }
+
+ public void setEmpPbYn(String empPbYn) {
+     this.empPbYn = empPbYn;
+ }
+
+ public String getEmpRemarks() {
+     return empRemarks;
+ }
+
+ public void setEmpRemarks(String empRemarks) {
+     this.empRemarks = empRemarks;
+ }
+
+ public String getRegUser() {
+     return regUser;
+ }
+
+ public void setRegUser(String regUser) {
+     this.regUser = regUser;
+ }
+
+ public String getRegDt() {
+     return regDt;
+ }
+
+ public void setRegDt(String regDt) {
+     this.regDt = regDt;
+ }
+
+ public String getUdtUser() {
+     return udtUser;
+ }
+
+ public void setUdtUser(String udtUser) {
+     this.udtUser = udtUser;
+ }
+
+ public String getUdtDt() {
+     return udtDt;
+ }
+
+ public void setUdtDt(String udtDt) {
+     this.udtDt = udtDt;
+ }
+
+ public String getUseYn() {
+     return useYn;
+ }
+
+ public void setUseYn(String useYn) {
+     this.useYn = useYn;
+ }
+
+ public String getEmpAcc() {
+     return empAcc;
+ }
+
+ public void setEmpAcc(String empAcc) {
+     this.empAcc = empAcc;
+ }
+
+ // EmpMaster 모델 복사
+ public void CopyData(EmpMaster param)
+ {
+     this.empNo = param.getEmpNo();
+     this.empNm = param.getEmpNm();
+     this.empGd = param.getEmpGd();
+     this.empPs = param.getEmpPs();
+     this.empDp = param.getEmpDp();
+     this.empJoinDt = param.getEmpJoinDt();
+     this.empResnDt = param.getEmpResnDt();
+     this.empHp = param.getEmpHp();
+     this.empEmail = param.getEmpEmail();
+     this.empRnum = param.getEmpRnum();
+     this.empPt = param.getEmpPt();
+     this.empTel = param.getEmpTel();
+     this.empAuthCd = param.getEmpAuthCd();
+     this.empPw = param.getEmpPw();
+     this.empBirth = param.getEmpBirth();
+     this.empSl = param.getEmpSl();
+     this.empZip = param.getEmpZip();
+     this.empAddr = param.getEmpAddr();
+     this.empAddr2 = param.getEmpAddr2();
+     this.empCrNo = param.getEmpCrNo();
+     this.empLc = param.getEmpLc();
+     this.empPbYn = param.getEmpPbYn();
+     this.empRemarks = param.getEmpRemarks();
+     this.regUser = param.getRegUser();
+     this.regDt = param.getRegDt();
+     this.udtUser = param.getUdtUser();
+     this.udtDt = param.getUdtDt();
+     this.useYn = param.getUseYn();
+     this.empAcc = param.getEmpAcc();
+ }
+}
